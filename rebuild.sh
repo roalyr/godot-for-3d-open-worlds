bf01a3697f7471f62a2a2d910fd56a10f04ef8d3
Roman <roal.yr@gmail.com>
2022-02-14 13:54:43 +0200

    MINOR: updater rebuild script for convenience.

--- rebuild.sh
+++ rebuild.sh
@@ -10,12 +10,18 @@ export SCONS_CACHE_LIMIT=10000;
 mkdir -p $SCONS_CACHE
 
 # Editor
-scons -j8 platform=x11;
+scons -j1 platform=x11;
 
 # Template(s)
-scons -j8 platform=x11 tools=no target=release bits=64;
-scons -j8 platform=android target=release_debug android_arch=armv7;
-scons -j8 platform=android target=release_debug android_arch=arm64v8;
+# Linux, removing debug symbols
+scons -j1 platform=x11 tools=no target=release bits=64;
+strip ./bin/godot.x11.opt.64
+
+# Android
+scons -j1 platform=android target=release_debug android_arch=armv7;
+scons -j1 platform=android target=release_debug android_arch=arm64v8;
 cd platform/android/java;
 ./gradlew generateGodotTemplates;
+
+
 
