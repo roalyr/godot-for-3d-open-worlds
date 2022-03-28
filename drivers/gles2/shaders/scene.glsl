b5c1bd6c64ac88e90744584e261a042cd5014c0e
Roman Yeremenko <roal.yr@gmail.com>
2022-03-28 15:03:30 +0300

    NEW: GLES2 Log depth added

--- drivers/gles2/shaders/scene.glsl
+++ drivers/gles2/shaders/scene.glsl
@@ -350,6 +350,9 @@ uniform mediump float fog_height_curve;
 
 #endif //fog
 
+// LOG DEPTH
+varying float vertex_pos_out;
+
 void main() {
 	highp vec4 vertex = vertex_attrib;
 
@@ -715,6 +718,12 @@ VERTEX_SHADER_CODE
 	gl_Position = position;
 #else
 	gl_Position = projection_matrix * vec4(vertex_interp, 1.0);
+	
+	// LOG DEPTH.
+	// https://outerra.blogspot.com/2012/11/maximizing-depth-buffer-range-and.html
+	// Position passed to fragment shader.
+	vertex_pos_out = gl_Position.w + 1.0;
+	
 #endif
 
 #if defined(RENDER_DEPTH) && defined(USE_RGBA_SHADOWS)
@@ -1600,7 +1609,30 @@ uniform mediump float fog_height_curve;
 #endif //vertex lit
 #endif //fog
 
+
+// LOG DEPTH
+// Far	Near (const)	C (const)	FC
+// 1.00E+19	0.01	0.01	0.025546734229603
+// 1.00E+20	0.01	0.01	0.0241274712168473
+// 1.00E+21	0.01	0.01	0.0228576043106975
+// 1.00E+22	0.01	0.01	0.0217147240951626
+// 1.00E+23	0.01	0.01	0.0206806896144406
+// 1.00E+24	0.01	0.01	0.0197406582683296
+// 1.00E+25	0.01	0.01	0.0188823687784023
+// 1.00E+26	0.01	0.01	0.0180956034126355
+// 1.00E+27	0.01	0.01	0.0173717792761301
+
+
+
+varying float vertex_pos_out;
+const float FC = 0.025546734229603; //For FAR = 1e19, C = 0.01 
+
+
 void main() {
+
+	// LOG DEPTH
+	gl_FragDepth = log(vertex_pos_out)*FC;
+
 #ifdef RENDER_DEPTH_DUAL_PARABOLOID
 
 	if (dp_clip > 0.0)
